ba60113839aef2dcdba744188faa7353ae369a62
evankhan0123 <evankhan1234@gmail.com>
2020-06-27 13:47:58 -0700

    ok

--- app/src/main/java/com/evan/bazar/ui/fragments/StepOneFragment.kt
+++ app/src/main/java/com/evan/bazar/ui/fragments/StepOneFragment.kt
@@ -134,10 +134,10 @@ class StepOneFragment : Fragment() {
 
     }
     fun showImage(temp:String?){
-        image_address="http://192.168.0.106/"+temp
+        image_address="http://hathbazzar.com/"+temp
         Log.e("for","Image"+temp)
         Glide.with(this)
-            .load("http://192.168.0.106/"+temp)
+            .load("http://hathbazzar.com/"+temp)
             .into(img_user_profile!!)
     }
 }
--- app/src/main/java/com/evan/bazar/ui/home/product/CreateProductFragment.kt
+++ app/src/main/java/com/evan/bazar/ui/home/product/CreateProductFragment.kt
@@ -455,10 +455,10 @@ class CreateProductFragment : Fragment(),KodeinAware,IUnitListener,ICategoryType
         progress_bar?.hide()
     }
     fun showImage(temp:String?){
-        image_address="http://192.168.0.106/"+temp
+        image_address="http://hathbazzar.com/"+temp
         Log.e("for","Image"+temp)
         Glide.with(this)
-            .load("http://192.168.0.106/"+temp)
+            .load("http://hathbazzar.com/"+temp)
             .into(img_background_mypage!!)
         img_user_add?.visibility=View.INVISIBLE
     }
